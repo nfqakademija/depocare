c60cf2264266d2b67dccf048856ba9f65eb14ffe
Haroldas Zapalskis <haroldas.zapalskis@gmail.com>
2018-04-11 00:34:12 +0300

    Fixses composer install working

--- depocare/config/bundles.php
+++ depocare/config/bundles.php
@@ -16,5 +16,5 @@ return [
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
-    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true]
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
 ];
