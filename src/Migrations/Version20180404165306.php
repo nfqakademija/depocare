72b2985396470a5302deb1e81c5690da104ac402
Liudas Kazalupskis <kazalupskis.liudas@gmail.com>
2018-04-13 17:36:13 +0300

    Fixed migration

--- src/Migrations/Version20180404165306.php
+++ src/Migrations/Version20180404165306.php
@@ -15,21 +15,21 @@ class Version20180404165306 extends AbstractMigration
         $this->addSql(
             'INSERT INTO `users` (`id`,`username`,`username_canonical`,
           `email`,`email_canonical`,`enabled`,`salt`,`password`,`last_login`,
-          `confirmation_token`,`password_requested_at`,`roles`,`lastname`,`balance`)
+          `confirmation_token`,`password_requested_at`,`roles`,`lastname`,`balance`, `firstname`)
           VALUES (1,\'test2\',\'test2\',\'test2@test.lt\',\'test2@test.lt\',1,NULL,
           \'$2y$13$wFpZ4RUvByINqyncCZ/TxOw00KFTVyOjwSDZtEqVbGVY5wvj9HJ92\',NULL,
-          NULL,NULL,\'a:0:{}\',\'Testinis\',3),
+          NULL,NULL,\'a:0:{}\',\'Testinis\',3, \'Test\'),
           (2,\'test\',\'test\',\'test@test.lt\',\'test@test.lt\',1,NULL,
           \'$2y$13$4nZ1ma5s28yl.pjKEEY3nuWaw3SLiGWN0l5lmE/dBuv/Hnwi26e8u\',NULL,
-          NULL,NULL,\'a:0:{}\',\'Pirminis\',22),
+          NULL,NULL,\'a:0:{}\',\'Pirminis\',22, \'Test2\'),
           (3,\'Liudas\',\'Liudas\',\'liudas@gmail.com\',\'liudas@gmail.com\',1,NULL,
           \'$2y$13$Ao0GlrNa9xZiFnN356WmU.5aDlZmYkUcVHM3ciM2gAgSZJY2HTaHu\',NULL
-          ,NULL,NULL,\'a:0:{}\',\'Kazalupskis\',14),
+          ,NULL,NULL,\'a:0:{}\',\'Kazalupskis\',14, \'Liudas\'),
           (4,\'Haroldas\',\'Haroldas\',\'haris@gmail.com\',\'haris@gmail.com\',1,NULL,
           \'$2y$13$4oej2cAnAlx5U3Fk52r85.1zB9NuE1Gtbbu72OlbXxmbwpM83U4fq\',NULL,
-          NULL,NULL,\'a:0:{}\',\'Zapalskis\',68);'
+          NULL,NULL,\'a:0:{}\',\'Zapalskis\',68, \'Haroldas\');'
         );
-        
+
         $this->addSql(
             "INSERT INTO `symfony`.`categories` (`id`, `title`) 
                 VALUES ('1', 'Vaikai'), ('2', 'Sveikata'), ('3', 'Gyvūnai ir gamta'), 
